ba26c9734b60264575d4ae26dbcc571393bb0aa2
Eviotie <104661931+Eviotie@users.noreply.github.com>
2024-03-13 16:13:16 -0700

    a

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import random
+from gtts import gTTS 
+import os
+
+tex = ["pizza", "icecream", "burger", "fries", "chicken", "pasta", "cake", "donut", "taco", "burrito"]
+# Language in which you want to convert 
+language = 'es'
+for i in tex:
+    x = input("Enter the text: ")
+    text = random.choice(tex)
+    myobj = gTTS(text=i, lang=language, slow=False) 
+    tex.remove(text)
+    myobj.save("welcome.mp3") 
+    os.system("welcome.mp3")
