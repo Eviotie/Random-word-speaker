47f2beabb5ba0c3a86ad0547b8509076506cda0c
Eviotie <104661931+Eviotie@users.noreply.github.com>
2024-03-14 15:58:02 -0700

    a

--- main.py
+++ main.py
@@ -3,25 +3,34 @@ from gtts import gTTS
 import os
 from pathlib import Path
 import pygame
-
+ 
 pygame.mixer.init()
-tex = ["¿Cómo te llamas?",
-"¿Cuántos años tienes?",
-"¿Cuándo es tu cumpleaños?",
-"¿Cúal es la fecha de hoy?",
-"¿Qué día es?",
-"¿Qué hora es?",
-"¿Qué te gusta hacer?",
-"¿Qué le gusta hacer a tu mejor amigo?",
-"¿Qué no te gusta hacer?"
-]
+def init_tex():
+    global tex
+    tex = ["¿Cómo te llamas?",
+    "¿Cuántos años tienes?",
+    "¿Cuándo es tu cumpleaños?",
+    "¿Cúal es la fecha de hoy?",
+    "¿Qué día es?",
+    "¿Qué hora es?",
+    "¿Qué te gusta hacer?",
+    "¿Qué le gusta hacer a tu mejor amigo?",
+    "¿Qué no te gusta hacer?"
+    ]
 # Language in which you want to convert 
 language = 'es'
-for i in range(len(tex)):
-    text = random.choice(tex)
-    myobj = gTTS(text=text, lang=language, slow=True) 
-    tex.remove(text)
-    myobj.save(f"welcome{i}.mp3")
-    pygame.mixer.music.load(f"welcome{i}.mp3")
-    pygame.mixer.music.play()
-    x = input("enter text")
+for j in range(5):
+    init_tex()
+    right = 0
+    for i in range(len(tex)):
+        text = random.choice(tex)
+        myobj = gTTS(text=text, lang=language, slow=False) 
+        tex.remove(text)
+        myobj.save(f"welcome{i}.mp3")
+        pygame.mixer.music.load(f"welcome{i}.mp3")
+        pygame.mixer.music.play()
+        x = input("enter text")
+        if x == "x":
+            right += 1
+    print(right)
+    print(j + 1)
