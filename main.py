e4a2f75602ef642e87b95ec1ff046c21d4e9d336
Eviotie <104661931+Eviotie@users.noreply.github.com>
2024-03-13 20:14:51 -0700

    a

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import random
 from gtts import gTTS 
 import os
+from pathlib import Path
+import pygame
 
-tex = ["pizza", "icecream", "burger", "fries", "chicken", "pasta", "cake", "donut", "taco", "burrito"]
+pygame.mixer.init()
+tex = ["¿Cómo te llamas?",
+"¿Cuántos años tienes?",
+"¿Cuándo es tu cumpleaños?",
+"¿Cúal es la fecha de hoy?",
+"¿Qué día es?",
+"¿Qué hora es?",
+"¿Qué te gusta hacer?",
+"¿Qué le gusta hacer a tu mejor amigo?",
+"¿Qué no te gusta hacer?"
+]
 # Language in which you want to convert 
 language = 'es'
-for i in tex:
-    x = input("Enter the text: ")
+for i in range(len(tex)):
     text = random.choice(tex)
-    myobj = gTTS(text=i, lang=language, slow=False) 
+    myobj = gTTS(text=text, lang=language, slow=True) 
     tex.remove(text)
-    myobj.save("welcome.mp3") 
-    os.system("welcome.mp3")
+    myobj.save(f"welcome{i}.mp3")
+    pygame.mixer.music.load(f"welcome{i}.mp3")
+    pygame.mixer.music.play()
+    x = input("enter text")
